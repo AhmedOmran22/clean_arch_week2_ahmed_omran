760d990d36c570171b31f5af736774d4cdb5a010
Ahmed Omran <mamdouhanahamada@gmail.com>
2025-09-23 21:25:22 +0300

    navigate to upgrade plan view

--- lib/core/widgets/custom_action_button_widget.dart
+++ lib/core/widgets/custom_action_button_widget.dart
@@ -1,3 +1,4 @@
+import 'package:clean_arch_week2_ahmed_omran/core/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:sizer/sizer.dart';
@@ -15,23 +16,28 @@ class CustomActionButtonWidget extends StatelessWidget {
   final Color color;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 40.w,
-      padding: const EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(36),
-      ),
-      child: Row(
-        spacing: 4,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SvgPicture.asset(svgImage),
-          Text(
-            label,
-            style: AppStyles.style15Regular.copyWith(color: Colors.white),
-          ),
-        ],
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, AppRoutes.upgradePlan);
+      },
+      child: Container(
+        width: 40.w,
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(36),
+        ),
+        child: Row(
+          spacing: 4,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(svgImage),
+            Text(
+              label,
+              style: AppStyles.style15Regular.copyWith(color: Colors.white),
+            ),
+          ],
+        ),
       ),
     );
   }
