35d322a1a9839224cfb825c8c524243400d26db3
Ahmed Omran <mamdouhanahamada@gmail.com>
2025-09-24 09:50:28 +0300

    prevent device oriantation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:clean_arch_week2_ahmed_omran/anime_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  preventDeviceOriantation();
   runApp(const AnimeApp());
+}
+
+preventDeviceOriantation() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 }
